c9152bd72e901c9c4ae0ce72592fdaff7131b28d
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-02 14:35:47 +0100

    Class Color added.

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    class Color
+    {
+        private byte red, green, blue, alpha;
+
+        // Constructor
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            red = r;
+            green = g;
+            blue = b;
+            alpha = a;
+        }
+
+        // Overload constructor
+        public Color(byte r, byte g, byte b)
+        {
+            red = r;
+            green = g;
+            blue = b;
+            alpha = 255;
+        }
+
+        // Getter methods 
+        public byte GetRed() => red;
+        public byte GetGreen() => green;
+        public byte GetBlue() => blue;
+        public byte GetAlpha() => alpha;
+
+        // Get the gray scale of the current color
+        public int GerGray() => (red + green + blue) / 3;
+
+
+    }
+}
