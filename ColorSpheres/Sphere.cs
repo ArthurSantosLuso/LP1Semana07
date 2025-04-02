718283d3d2d3e0e457926f38545166b935a4a524
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-02 14:44:31 +0100

    Class Sphere added.

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    class Sphere
+    {
+        private Color color;
+        private float radius;
+        private int qtdThrow;
+
+        public Sphere(Color sphereColor, float sphereRadius)
+        {
+            color = sphereColor;
+            radius = sphereRadius;
+            qtdThrow = 0;
+        }
+
+        public void Pop() => radius = 0;
+
+        public int GetTimesThrow() => qtdThrow;
+
+        public void Throw()
+        {
+            if (radius > 0)
+                qtdThrow++;
+        }
+    }
+}
