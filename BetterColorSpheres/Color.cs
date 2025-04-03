8b9bb6f1eb6eebf4d33c2ed690015ed611890c99
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-03 14:18:18 +0100

    Exercicio 5 inicializado. Class Color com propriedades feita.

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterColorSpheres
+{
+    class Color
+    {
+        private byte Red { get; }
+        private byte Green { get; }
+        private byte Blue { get; }
+        private byte Alpha { get; }
+        private byte Gray
+        {
+            get
+            {
+                return (byte)((Red + Green + Blue) / 3);
+            }
+        }
+
+        // Constructor
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+            Alpha = a;
+        }
+
+        // Overload constructor
+        public Color(byte r, byte g, byte b)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+            Alpha = 255;
+        }
+    }
+}
