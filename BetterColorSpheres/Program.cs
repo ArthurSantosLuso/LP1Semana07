3131b5f7906e88b2dc4c7104bdb67110f9f17b26
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-03 14:28:42 +0100

    Program.cs altered, exercice finalized

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -6,7 +6,26 @@ namespace BetterColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Sphere esfera1 = new Sphere(new Color(255, 255, 255), 5);
+            Sphere esfera2 = new Sphere(new Color(0, 255, 255), 2);
+            Sphere esfera3 = new Sphere(new Color(255, 0, 0), 1);
+
+            esfera1.Throw();
+            esfera1.Throw();
+            esfera1.Throw();
+
+            esfera2.Throw();
+            esfera2.Throw();
+
+            esfera3.Throw();
+            esfera3.Pop();
+            esfera3.Throw();
+
+
+
+            Console.WriteLine($"Sphere 1:\n{esfera1}");
+            Console.WriteLine($"Sphere 2:\n{esfera2}");
+            Console.WriteLine($"Sphere 3:\n{esfera3}");
         }
     }
 }
