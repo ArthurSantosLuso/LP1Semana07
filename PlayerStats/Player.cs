9b7bf67a4055f3cf135ffdace9b6e38c3953b8d8
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-04 12:45:49 +0100

    Classe player, propriedades e variaveis de instancia adicionadas ao projeto.

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerStats
+{
+    class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames;
+
+        public float HighScore
+        {
+            get => highScore;
+
+            set
+            {
+                if (highScore < value)
+                    highScore = value;
+            }
+        }
+        public string Name { get; set; }
+        public float WinRate
+        {
+            get
+            {
+                if (playedGames > 0)
+                    return wonGames / playedGames;
+                else
+                    return 0;
+            }
+        }
+    }
+}
